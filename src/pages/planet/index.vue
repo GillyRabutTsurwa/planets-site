6cc6046fc6f3e43e1969168516bfc1d9e75a1885
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-02-23 00:44:34 -0500

    Rendering, Analysing and Formatting Single Page Data - specifically with images - i am preparing them in a way that i will change them seamlessly later on

--- src/pages/planet/index.vue
+++ src/pages/planet/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {useRoute} from "vue-router";
+import { useRoute } from "vue-router";
 import { onMounted, toRef } from "vue";
 import useFilterPlanet from "@/composables/filterPlanet";
 
@@ -8,25 +8,44 @@ const slug = route.params.slug;
 console.log(route.params);
 
 const { state, filterPlanette } = useFilterPlanet();
-const { name } = state.selectPlanet;
 
 // TESTING
-const x = toRef(state, "selectPlanet");
+// const x = toRef(state, "selectPlanet");
+
 filterPlanette(slug);
 console.log(state.selectPlanet);
-const { overview } = state.selectPlanet;
-console.log(overview)
-onMounted(() => {
-})
+const { geology, images, name, overview, radius, revolution, rotation, structure, temperature } = state.selectPlanet;
+
+//NOTE: i am accessing images.geology separately and don't need it in the obejct used to render the main and internal images
+//NOTE: therefore i will make a new object that does NOT contain the geology property
+
+//NOTE: making a new duplicate object of images called updated images so that images stays unaffected through property deletion
+const updatedImages = { ...images };
+// NOTE; deleting the property from our new duplicate object
+delete updatedImages.geology
+//TESTING to see if it works
+console.log(updatedImages);
+//PASS
+
+
+const getImgURL = (imageName) => {
+    return new URL(`../../assets/img/${imageName}`, import.meta.url);
+}
 
 
 </script>
 
 <template>
-    <h2>{{ slug }}</h2>
-    <h4 v-for="(value, propertyName) in state.selectPlanet" :key="propertyName">
-        <span></span> {{ propertyName }}: {{ value }}
-    </h4>
+    <div class="planet-container">
+        <figure>
+            <!-- then in the template, i am using updatedImages, instead of images -->
+            <img v-for="(currentImage, currentProperty) in updatedImages" :key="currentImage" :src="getImgURL(currentImage)"
+                :alt="currentProperty">
+            <!-- this below element will be my "pseudoelement" -->
+            <img v-if="images.geology" :src="getImgURL(images.geology)" alt="">
+        </figure>
+    </div>
+    <h2>{{ name }}</h2>
 </template>
 
 
