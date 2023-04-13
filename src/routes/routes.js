6c36b8787ea34c24cb0169f846e8db9b2f7a04f8
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-04-12 23:00:33 -0400

    Renaming Dynamic Directory

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,11 @@
 import Home from "@/pages/index.vue";
 import About from "@/pages/about/index.vue";
-import Planet from "@/pages/planet/index.vue";
+import Planet from "@/pages/[slug]/index.vue";
 
 const routes = [
-  { name: "Home", path: "/", component: Home },
-  { name: "About", path: "/about", component: About },
-  { name: "Planet", path: "/:slug", component: Planet },
-  {},
+    { name: "Home", path: "/", component: Home },
+    { name: "About", path: "/about", component: About },
+    { name: "Planet", path: "/:slug", component: Planet },
 ];
 
 export default routes;
